55261750f8385acf800d2a3f7e5ddbc3685a548a
Caden Johns <cjohns1@mtech.edu>
2026-09-02 15:47:53 -0600

    Complete Challenge 1

--- src/main.c
+++ src/main.c
@@ -1,3 +1,8 @@
+// Caden Johns
+// CSCI 232 Fall 2026
+// Programming Assignment #1
+// I declare that I am the author of this work, take full responsibility for it, and have disclosed any material external assistance.
+
 #define clearBuffer() while (getchar() != '\n');
 #include <stdio.h>
 #include <stdbool.h>
